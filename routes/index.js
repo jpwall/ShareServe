2273e8948ef1abaeb24b81818b7a8c5b271c979d
jpwall <jessephilipwalling@gmail.com>
2017-03-29 12:25:51 +0000

    create route at /listings/ for listings page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/listings/', function(req, res, next) {
+    res.render('listings');
 });
 
 module.exports = router;
