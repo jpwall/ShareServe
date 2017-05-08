9c9691e08f5af510ec96aad77475b25fd246b42a
jpwall <jessephilipwalling@gmail.com>
2017-05-08 11:08:02 +0000

    update index.js to include newer pages

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,20 @@ router.get('/listings/', function(req, res, next) {
     res.render('listings');
 });
 
+router.get('/newListing/', function(req, res, next) {
+    res.render('newListing');
+});
+
 router.get('/login/', function(req, res, next) {
     res.render('login');
+});
+
+router.get('/user/dashboard/', function(req, res, next) {
+    res.render('dashboard');
+});
+
+router.get('/user/reviews/', function(req, res, next) {
+    res.render('reviews');
 });
 
 module.exports = router;
